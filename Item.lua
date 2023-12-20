5331d6f93a45ec02855c97bb85647f1fe687e07e
G. van Dorland <114250309+ExercitusMortem@users.noreply.github.com>
2023-12-20 20:46:21 +0100

    Update Item.lua

--- Item.lua
+++ Item.lua
@@ -1,15 +1,15 @@
 local UID = require(game.ReplicatedStorage.UID)
 local Object = require(game.ReplicatedStorage.Object)
+local Interactable = require(game.ReplicatedStorage.Interactable)
 local Item = {}
 
 local Item = {}
 
-Item.new = function()
+Item.new = function(item_id : string, pos : Vector3)
 	local self = {}
-
-	local object = Object.new()
-
 	
+	local OBJECT = Object.new(pos)
+	local INTERACTABLE = Interactable.new(OBJECT)
 	return self
 end
 
