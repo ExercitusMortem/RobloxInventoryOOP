91166584d1038c7fff5991a822dc77d49b33391c
G. van Dorland <114250309+ExercitusMortem@users.noreply.github.com>
2024-01-21 16:11:57 +0100

    Update ItemManager.lua

--- ItemManager.lua
+++ ItemManager.lua
@@ -1,23 +1,33 @@
+local Database = require(game.ReplicatedStorage.Database)
 local Item = require(game.ReplicatedStorage.Item)
-local Items = {}
+local Events = game.ReplicatedStorage.Events
 
 local ItemManager = {}
+local Items = {}
 
-function ItemManager:GetItems()
-	return Items
+
+function ItemManager.newItem(item_id, item_qt, pos)
+	local item = Item.new(item_id, item_qt, pos)
+	Items[item.UID] = item 
+	return 
 end
 
-function ItemManager:CreateItem(...)
-	local item = Item.new(...)
-	ItemManager[item._UID]=item
+function ItemManager.removeItem(UID)
+	if Items[UID] and Items[UID].item_qt then
+		Items[UID].remove()
+	end
 end
 
-function ItemManager:DestroyItem(UID)
-	Items[UID]=nil
-end
-
-function ItemManager:GetItemFromUID(UID)
+function ItemManager.fetchItem(UID)
 	return Items[UID]
 end
+
+Events.ItemManager.Event:Connect(function(...)
+	local args = {...}
+
+	local command = table.remove(args,1)
+	
+	ItemManager[command](table.unpack(args))
+end)
 
 return ItemManager
