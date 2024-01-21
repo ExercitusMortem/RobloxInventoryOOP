07b244cee1af4541595e018a640ac600f08b06df
G. van Dorland <114250309+ExercitusMortem@users.noreply.github.com>
2024-01-21 16:11:13 +0100

    Update Interactable.lua

--- Interactable.lua
+++ Interactable.lua
@@ -1,33 +1,31 @@
 local Interactable = {}
-Interactable.__index = Interactable
 
-local function generate(part)
+local function generatePart(pos)
+	local part = Instance.new("Part")
+	part.Anchored = true
+	part.Size = Vector3.new(1,1,1)
+	part.Position = pos or Vector3.new(0,0,0)
+	
 	local proximityPrompt = Instance.new("ProximityPrompt",part)
 	proximityPrompt.RequiresLineOfSight = false
 	
-	return proximityPrompt
+	return part, proximityPrompt
 end
 
-Interactable.new = function(part : Part, callbackFunction)
-	local self = setmetatable({},Interactable)
+Interactable.new = function(callback, pos)
+	local self = {}
 	
-	local proximityPrompt = generate(part)
-
-	local callback = callbackFunction or function() print("Default callback") end
+	self.part, self.proximityPrompt = generatePart(pos)
 	
-	local function interacted(player_who_triggered)
-		print("Interacted!")
-		callback(player_who_triggered, self)
+	local function fireCallback(...)
+		callback(...)
 	end
 	
-	function self:setCallback(callbackFunction)
-		callback = callbackFunction
-		print("Set callback")
-	end
+	self.proximityPrompt.Triggered:Connect(function(playerWhoClicked)
+		fireCallback(self, playerWhoClicked)
+	end)
 	
-	proximityPrompt.Triggered:Connect(interacted)
-	
-	return self	
+	return self
 end
 
 return Interactable
