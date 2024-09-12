ceaf55364ab34b16e87427c4ece06d44dae00b14
G. van Dorland <giodor13@gmail.com>
2024-09-12 20:02:25 +0200

    Update Item.lua

--- Item.lua
+++ Item.lua
@@ -1,75 +1,81 @@
 local Interactable = require(game.ReplicatedStorage.Interactable)
-local Database = require(game.ReplicatedStorage.Database)
-local UID = require(game.ReplicatedStorage.UID)
-local Types = require(game.ReplicatedStorage.Types)
 
-local Events = game.ReplicatedStorage.Events
+local InstanceFolder = workspace:FindFirstChild('InstanceFolder') or Instance.new('Folder', workspace) InstanceFolder.Name = 'InstanceFolder'
 
+local i = 0
+local function UID()
+	i += 1
+	return i
+end
+local ItemInstances = {}
 
-local Item = {}
+function ItemInstances.Get(UID_or_Model)
+	local object_UID = typeof(UID_or_Model)=='number' and UID_or_Model or UID_or_Model:GetAttribute('UID')
+	return ItemInstances[object_UID]
+end
 
-Item.new = function(item_id, item_qt, position)
+local ItemInstance = {}
+
+function ItemInstance.new(item, quantity)
+	local self = {}
 	
-	local model = Instance.new("Model")
-	
-	local self : Types.ItemType = {
-		item_id=item_id or 'DEFAULT',
-		item_qt=item_qt or 1,
-		UID = UID(),
-		remove = function() model:Destroy() end,
-		setQuantity = function() print(' dumb ') end,
-	}
-	
-	local position = position or Vector3.new(0,0,0)
+	self._item = item
+	self._UID = UID()
+	self.quantity = quantity
 	
 	local is_spawned = false
 	
-	local update
+	local viewModel = item._model:Clone()
+	viewModel:SetAttribute('UID', self._UID)
 
-	local function toggleSpawned(optional_state : boolean)
-		if optional_state ~= nil then
-			is_spawned = optional_state
+	function self:ToggleSpawned(is_toggled : boolean?)
+		if is_toggled ~= nil then
+			is_spawned = is_toggled
 		else
 			is_spawned = not is_spawned
 		end
-		update()
+		viewModel.Parent = is_spawned and InstanceFolder or nil
 	end
 	
-	local function pickupItem(player)
-		print(player.Name .. " is attempting to pick up item with UID " .. self.UID)
-		Events.InventoryManager:Fire('addItemToOwner',player,self)
-		toggleSpawned(false)
+	function self:PivotTo(cfr : CFrame)
+		viewModel:PivotTo(cfr)
 	end
 	
-	local function dropItem(player)
-		print(player.Name .. " is attempting to drop item with UID " .. self.UID)
-		position = player.Character.HumanoidRootPart.Position - Vector3.new(0,3,0)
-		toggleSpawned(true)
-	end
+	local interact = Interactable.new(self,viewModel,function(itemInstance, player)
+		print(player.Name .. ' interacted with ' .. self._item._name .. '[' .. self._UID .. ']')
+		self:ToggleSpawned(false)
+	end)
 	
-	self.setQuantity = function(qt)
-		self.item_qt = qt
-		print(qt, self.item_qt)
-		update()
-		if qt == 0 then
-			print('oit')
-			Events.ItemManager:Fire('removeItem', self.UID)
-		end
-		return self
+	ItemInstances[self._UID]=self
+	
+	return self
+end
+
+local Items = {}
+function Items.Get(name)
+	return Items[name]
+end
+
+local Item = {}
+
+function Item.new(name, desc, model)
+	if Items[name] then warn("Item of _name \'" .. name .. '\' already exists, returning it..' ) return Items[name] end
+	local self = setmetatable({},Item)
+	self._name = name
+	self._desc = desc
+	self._model = model
+
+	function self:Create(quantity, cfr : CFrame)
+		local instance = ItemInstance.new(self, quantity)
+		instance:PivotTo(cfr)
+		instance:ToggleSpawned(true)
+		return Instance
 	end
 	
 
-	local interactable = Interactable.new(function(self, playerWhoClicked) pickupItem(playerWhoClicked) end, position)
-	interactable.part.Parent = model
-	
-	update = function()
-		model.Parent = is_spawned and workspace or nil
-		model:PivotTo(CFrame.new(position))
-		interactable.proximityPrompt.ObjectText = Database[self.item_id].name .. " x" .. self.item_qt
-	end
-	
-	toggleSpawned(true)
-	
+	Items[name] = self
+
+		
 	return self
 end
 
